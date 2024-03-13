cb52cd40086ec3f7bf81e678411b218c2ac9424a
Sanghoon Yoon <shygiants@gmail.com>
2024-03-13 20:11:07 +0900

    Added api-server

---
+++ api-server/src/main/java/io/github/shygiants/sirenorder/SirenorderApplication.java
@@ -0,0 +1,13 @@
+package io.github.shygiants.sirenorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirenorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirenorderApplication.class, args);
+	}
+
+}
---
+++ api-server/src/test/java/io/github/shygiants/sirenorder/SirenorderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shygiants.sirenorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirenorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

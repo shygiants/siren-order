69841584f154a1edd55a9c9c997dfda490fc94fc
Sanghoon Yoon <shygiants@gmail.com>
2024-04-18 00:57:31 +0900

    Added equals and hashcode to cafe

--- api-server/src/main/java/io/github/shygiants/sirenorder/domain/entity/Cafe.java
+++ api-server/src/main/java/io/github/shygiants/sirenorder/domain/entity/Cafe.java
@@ -3,12 +3,16 @@ package io.github.shygiants.sirenorder.domain.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EqualsAndHashCode
 public class Cafe {
     @Id
     private Long id;
